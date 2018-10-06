a148e385ab446b9f97e4020e81326b7924b1f434
SCrawford81299 <thestevencrawford@gmail.com>
2018-10-06 17:31:49 -0600

    Created GameLogic class

---
+++ ProjectNIM/ProjectNIM/GameLogic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectNIM
+{
+    class GameLogic
+    {
+        public List<int> Piles { get; set; }
+
+        public string[] Players { get; set; }
+
+        public string ActivePlayer { get; set; }
+
+        public int RobotPileChoice()
+        {
+            //Daniel
+            throw new NotImplementedException();
+        }
+
+        public int RobotPieceChoice()
+        {
+            //Daniel
+            throw new NotImplementedException();
+        }
+
+        public void TakeFromPile(int pile, int pieces)
+        {
+            //Steven
+            throw new NotImplementedException();
+        }
+
+        public bool IsGameOver()
+        {
+            //Furman
+            throw new NotImplementedException();
+        }
+
+        public void InitializePiles(int difficulty)
+        {
+            //Furman
+            throw new NotImplementedException();
+        }
+
+        private void SwitchActivePlayer()
+        {
+            //Steven
+            throw new NotImplementedException();
+        }
+
+
+    }
+}

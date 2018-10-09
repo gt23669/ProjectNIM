bce4ddb87d616777cf3a77707cae979426555fbc
Fermin <ferminsandoval950@hotmail.com>
2018-10-09 13:04:03 -0600

    Added restart button while in game

--- ProjectNIM/ProjectNIM/Game.xaml.cs
+++ ProjectNIM/ProjectNIM/Game.xaml.cs
@@ -129,5 +129,23 @@ namespace ProjectNIM
         {
             btnSubmit.IsEnabled = true;
         }
+
+        private void btnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            logic.InitializePiles(logic.Difficulty);
+            logic.ActivePlayer = logic.Players[0];
+
+            ugrdGame.Children.Clear();
+
+            foreach (var pile in logic.Piles)
+            {
+                lblGameInfo.Content = $"{logic.ActivePlayer}'s turn!";
+                Pile temp = new Pile(pile);
+                temp.MouseLeftButtonDown += fillCbx;
+                temp.MouseLeftButtonDown += SelectColor;
+                ugrdGame.Children.Add(temp);
+                Piles.Add(temp);
+            }
+        }
     }
 }
